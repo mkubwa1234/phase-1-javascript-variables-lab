d38c035daa3ec6bb9b68519fc7a65bd5aba08793
Alpha <alpha.matara@student.moringaschool.com>
2024-03-19 11:28:56 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+
+const companyName = 'Scuber'
+console.log(companyName)
+let mostProfitableNeighborhood = 'Chelsea'
+console.log(mostProfitableNeighborhood)
+let companyCeo = 'Susan Smith'
